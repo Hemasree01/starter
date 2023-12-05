a5f6e6bf30f3bb0cd68319588382ab8df6142307
Hemasree01 <onmanihemasree@gmail.com>
2023-12-05 14:43:40 -0500

    new

---
+++ assignment.js
@@ -0,0 +1,6 @@
+let country = "India";
+let continent = "Asia";
+let population = 496461;
+console.log(country);
+console.log(continent);
+console.log(population);
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 let js = "amazing";
 if (js == "amazing") alert("javscript is fun");
 console.log(50 + 40 + 60);
+console.log("hema");
