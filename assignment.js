6848da5e9039cbcd3aca677378d1974daf6cab4b
Hemasree01 <onmanihemasree@gmail.com>
2023-12-05 15:43:48 -0500

    changes

--- assignment.js
+++ assignment.js
@@ -1,6 +1,24 @@
+// assignment 1
 let country = "India";
 let continent = "Asia";
 let population = 496461;
 console.log(country);
 console.log(continent);
 console.log(population);
+
+//asignment 2
+let isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+//assignment 3
+language = "telugu";
+const finland = 600000;
+let description =
+  "Portugal is in Europe, and its 11 million people speak portuguese";
+console.log(population / 2);
+console.log((population += 1));
+console.log(population > finland);
